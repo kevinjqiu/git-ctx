b6e80ce29e79f5f1eae2b348fe57dd6dccf62a4b
Kevin Qiu <kevin.qiu@instacart.com>
2024-05-16 16:56:48 -0400

    wip

--- src/tui/app.rs
+++ src/tui/app.rs
@@ -1,167 +1,123 @@
-use ratatui::layout::Alignment;
-use ratatui::prelude::{Color, Rect, Style};
-use ratatui::widgets::Paragraph;
-use tuirealm::command::{Cmd, CmdResult};
-use tuirealm::props::{Borders, TextModifiers};
-use tuirealm::{
-    Application, AttrValue, Attribute, Frame, MockComponent, NoUserEvent, Props, State, StateValue,
+use crossterm::{
+    event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyboardEnhancementFlags},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 
-struct Counter {
-    props: Props,
-    states: OwnStates,
+use ratatui::{
+    prelude::*,
+    widgets::{block::Position, Block, Borders, Paragraph},
+};
+use std::io::{self, stdout, Stdout};
+
+use ratatui::{
+    backend::CrosstermBackend,
+    buffer::Buffer,
+    layout::Rect,
+    widgets::{block::Title, Widget},
+    Frame, Terminal,
+};
+
+#[derive(Debug, Default)]
+pub struct App {
+    counter: u8,
+    exit: bool,
 }
 
-impl Default for Counter {
-    fn default() -> Self {
-        return Self {
-            props: Props::default(),
-            states: OwnStates::default(),
-        };
-    }
-}
+pub type Tui = Terminal<CrosstermBackend<Stdout>>;
 
-impl Counter {
-    pub fn label<S>(mut self, label: S) -> Self
-    where
-        S: AsRef<str>,
-    {
-        self.attr(
-            Attribute::Title,
-            AttrValue::Title((label.as_ref().to_string(), Alignment::Center)),
-        );
-        self
-    }
-
-    pub fn value(mut self, n: isize) -> Self {
-        self.attr(Attribute::Value, AttrValue::Number(n));
-        self
-    }
-
-    pub fn alignment(mut self, a: Alignment) -> Self {
-        self.attr(Attribute::TextAlign, AttrValue::Alignment(a));
-        self
-    }
-
-    pub fn foreground(mut self, c: Color) -> Self {
-        self.attr(Attribute::Foreground, AttrValue::Color(c));
-        self
-    }
-
-    pub fn background(mut self, c: Color) -> Self {
-        self.attr(Attribute::Background, AttrValue::Color(c));
-        self
-    }
-
-    pub fn modifiers(mut self, m: TextModifiers) -> Self {
-        self.attr(Attribute::TextProps, AttrValue::TextModifiers(m));
-        self
-    }
-
-    pub fn borders(mut self, b: Borders) -> Self {
-        self.attr(Attribute::Borders, AttrValue::Borders(b));
-        self
-    }
-}
-
-impl MockComponent for Counter {
-    fn view(&mut self, frame: &mut Frame, area: Rect) {
-        // Check if visible
-        if self.props.get_or(Attribute::Display, AttrValue::Flag(true)) == AttrValue::Flag(true) {
-            // Get properties
-            let text = self.states.counter.to_string();
-            let alignment = self
-                .props
-                .get_or(Attribute::TextAlign, AttrValue::Alignment(Alignment::Left))
-                .unwrap_alignment();
-            let foreground = self
-                .props
-                .get_or(Attribute::Foreground, AttrValue::Color(Color::Reset))
-                .unwrap_color();
-            let background = self
-                .props
-                .get_or(Attribute::Background, AttrValue::Color(Color::Reset))
-                .unwrap_color();
-            let modifiers = self
-                .props
-                .get_or(
-                    Attribute::TextProps,
-                    AttrValue::TextModifiers(TextModifiers::empty()),
-                )
-                .unwrap_text_modifiers();
-            let title = self
-                .props
-                .get_or(
-                    Attribute::Title,
-                    AttrValue::Title((String::default(), Alignment::Center)),
-                )
-                .unwrap_title();
-            let borders = self
-                .props
-                .get_or(Attribute::Borders, AttrValue::Borders(Borders::default()))
-                .unwrap_borders();
-            let focus = self
-                .props
-                .get_or(Attribute::Focus, AttrValue::Flag(false))
-                .unwrap_flag();
-            frame.render_widget(
-                Paragraph::new(text)
-                    .block(get_block(borders, title, focus))
-                    .style(
-                        Style::default()
-                            .fg(foreground)
-                            .bg(background)
-                            .add_modifier(modifiers),
-                    )
-                    .alignment(alignment),
-                area,
-            );
+impl App {
+    pub fn run(&mut self, terminal: &mut Tui) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.render_frame(frame))?;
+            self.handle_events()?;
         }
+        Ok(())
     }
 
-    fn query(&self, attr: Attribute) -> Option<AttrValue> {
-        self.props.get(attr)
+    fn render_frame(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.size());
     }
 
-    fn attr(&mut self, attr: Attribute, value: AttrValue) {
-        self.props.set(attr, value);
-    }
-
-    fn state(&self) -> State {
-        State::One(StateValue::Isize(self.states.counter))
-    }
-
-    fn perform(&mut self, cmd: Cmd) -> CmdResult {
-        match cmd {
-            Cmd::Submit => {
-                self.states.incr();
-                CmdResult::Changed(self.state())
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
             }
-            _ => CmdResult::None,
+            _ => {}
+        }
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Left => self.decrement_counter(),
+            KeyCode::Right => self.increment_counter(),
+            _ => {}
         }
     }
-}
 
-struct OwnStates {
-    counter: isize,
-}
+    fn exit(&mut self) {
+        self.exit = true;
+    }
 
-impl Default for OwnStates {
-    fn default() -> Self {
-        return Self { counter: 0 };
+    fn increment_counter(&mut self) {
+        self.counter += 1;
+    }
+
+    fn decrement_counter(&mut self) {
+        self.counter -= 1;
     }
 }
 
-impl OwnStates {
-    fn incr(&mut self) {
-        self.counter += 1
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        // let title = Title::from(" Counter App Tutorial ".bold());
+        let instructions = Title::from(Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]));
+
+        let block = Block::default()
+            // .title(title.alignment(Alignment::Center))
+            .title(
+                instructions
+                    .alignment(Alignment::Center)
+                    .position(Position::Bottom),
+            );
+
+        let counter_text = Text::from(vec![Line::from(vec![
+            "Value: ".into(),
+            self.counter.to_string().yellow(),
+        ])]);
+
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
     }
 }
 
-#[derive(Debug, Eq, PartialEq, Clone, Hash)]
-pub enum Id {
-    DigitCounter,
-    LetterCounter,
+fn init() -> io::Result<Tui> {
+    // execute!(stdout(), EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    Terminal::new(CrosstermBackend::new(stdout()))
 }
 
-fn init_app() -> Application<Id, Msg, NoUserEvent> {}
+fn restore() -> io::Result<()> {
+    // execute!(stdout(), LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
+}
+
+pub fn run_tui() -> io::Result<()> {
+    let mut terminal = init()?;
+    let app_result = App::default().run(&mut terminal);
+    restore()?;
+    app_result
+}
